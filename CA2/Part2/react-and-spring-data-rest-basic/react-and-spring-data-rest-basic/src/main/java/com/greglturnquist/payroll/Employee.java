0c4aedbbb195c7fb2d7613b5272d3a9dfbc1faa9
Ana Pinto <1231822@isep.ipp.pt>
2024-04-04 11:50:09 +0100

    Close #24. Close #25. Close #26. Commit that created the branch, copied the spring boot folder and then copied the src from CA1 to CA2/part2

---
+++ CA2/Part2/react-and-spring-data-rest-basic/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Entity // <1>
+public class Employee {
+
+	private @Id @GeneratedValue Long id; // <2>
+	private String firstName;
+	private String lastName;
+	private String description;
+	private String jobTitle;
+	private int jobYears;
+	private String emailField;
+
+	protected Employee() {}
+
+	public Employee(String firstName, String lastName, String description, String jobTitle, int jobYears, String emailField) throws InstantiationException {
+		if (firstName == null || firstName.isEmpty() || firstName.trim().isEmpty() || lastName == null || lastName.isEmpty() || jobTitle == null || jobTitle.isEmpty() || jobTitle.trim().isEmpty() || description == null || description.isEmpty() || description.trim().isEmpty() || jobYears < 0 || emailField == null || emailField.isEmpty() || emailField.trim().isEmpty() || !emailField.contains("@")){
+			throw new InstantiationException("Invalid employee");
+		}
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+		this.jobTitle = jobTitle;
+		this.jobYears = jobYears;
+		this.emailField = emailField;
+
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) &&
+			Objects.equals(firstName, employee.firstName) &&
+			Objects.equals(lastName, employee.lastName) &&
+			Objects.equals(description, employee.description) &&
+			Objects.equals(jobTitle, employee.jobTitle) &&
+			Objects.equals(jobYears, employee.jobYears) &&
+			Objects.equals(emailField, employee.emailField);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(id, firstName, lastName, description, jobTitle, jobYears, emailField);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public int getJobYears() {
+		return jobYears;
+	}
+
+	public void setJobYears(int jobYears) {
+		this.jobYears = jobYears;
+	}
+
+	public String getEmailField() {
+		return emailField;
+	}
+
+	public void setEmailField(String emailField) {
+		this.emailField = emailField;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+			"id=" + id +
+			", firstName='" + firstName + '\'' +
+			", lastName='" + lastName + '\'' +
+			", description='" + description + '\'' +
+			", jobTitle='" + jobTitle + '\'' +
+			", jobYears='" + jobYears + '\'' +
+			", emailField='" + emailField + '\'' +
+			'}';
+	}
+}
+// end::code[]
